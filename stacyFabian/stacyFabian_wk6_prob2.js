53bda61a2c1fcbc33a25e8b58440b4046e3959c9
Stacy Fabian <stacy.fabian@gmail.com>
2014-10-15 09:55:07 -0700

    Finally finished with prob 2 (the zoo)

--- stacyFabian/stacyFabian_wk6_prob2.js
+++ stacyFabian/stacyFabian_wk6_prob2.js
@@ -1,28 +1,25 @@
 // Stacy Fabian
 // Week 6 prob 2
 
-
 function Animal(name) {
 	this.name = name;
-}
+};
 
 Animal.prototype.move = function() {
 	return "walk";
 };
 
-
-function Bird() {
-	// insert name call through animal
-}
-
+function Bird(name) {
+	Animal.call(this,name);
+};
 Bird.prototype = new Animal;
 Bird.prototype.constuctor = Bird;
 
 Bird.prototype.move = function() {return "fly";};
 Bird.prototype.hasWings = true;
 
-function Fish() {
-	// insert name call through animal
+function Fish(name) {
+	Animal.call(this,name)
 }
 Fish.prototype = new Animal;
 Fish.prototype.constuctor = Fish;
@@ -30,16 +27,11 @@ Fish.prototype.constuctor = Fish;
 Fish.prototype.move = function() {return "swim";};
 
 
-function Penguin() {
-	// insert name call through animal
-}
+function Penguin(name) {
+	Animal.call(this,name);
+};
 Penguin.prototype = new Bird;
 Penguin.prototype.constructor = Penguin;
-
-Penguin.prototype.move = function() {return "swim";};
-
-// failed tests
-// pengo.name; // function() {return name;}
-// pengo instanceof Animal; // false
+Penguin.prototype.move = function() {return "swim";};x
 
 
