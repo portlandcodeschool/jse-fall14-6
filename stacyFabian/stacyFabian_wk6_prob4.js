28d0eaf7233ffac67f11698e18b2f365e57bcd5e
Stacy Fabian <stacy.fabian@gmail.com>
2014-10-15 09:23:40 -0700

    Adding invoice and what's done of prob 4

---
+++ stacyFabian/stacyFabian_wk6_prob4.js
@@ -0,0 +1,17 @@
+// Stacy Fabian
+// Week 6 prob 4
+
+- build the game board
+	- a single row (for now)
+	- function MemoryBoard
+		- takes 3 arguments
+			- array of game "cards"
+				- must be even number
+				- include a property
+			- callback to compare the match for true or false 
+				- 'match' depends on the card type
+			
+
+- build the card set (array)
+	- assign each card a unique identifier
+	- include some sort of property that indicates the match or not
