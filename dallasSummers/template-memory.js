1efdc496de4a9d1e41d67ad1e9e73be876dd7d2c
Dallasss7 <mr.dalsummers@gmail.com>
2014-10-18 07:45:06 -0700

    finished, finally, the memory game constructor

--- dallasSummers/template-memory.js
+++ dallasSummers/template-memory.js
@@ -1,60 +1,127 @@
 var MemoryBoard = (function() {
+
 	function Ctor(values,matchFn,displayFn,endgameFn) {
 
-		this.reset = function(){}
+		var faceupValuesArr = [];
 
+		board = values.slice();
+
+		this.reset = function(){
+
+			//create new game board
+			board = values.slice();
+			
+			//shuffling the entire array
+			board.sort(shuffle);
+
+			faceupValuesArr = []
+
+			return 'ready to play!';
+
+		};
+
+		
 		this.faceupWhere = function(){
 
-			return values[]
-		}
+			if(faceupValuesArr[0] !== undefined){
 
+				return board.indexOf('face up');
+			}
+			// return false;
+		};
 		
 		
 		this.faceupValue = function(){
 
-			if(faceupValuesArr[0]){
+			if(faceupValuesArr.length > 0){
 			
 				return faceupValuesArr[0];
 			}
 			
 			return false;
-		}
+		};
 
-		this.remaining = function(){}
+		this.remaining = function(){
+			return Object.keys(board).map(Number)
+		};
 
 		
-
 		this.lift = function(where){
+	
+			var lifted = board[where];
+	
+			if(lifted !== undefined){
+				
+				if(faceupValuesArr[0] === undefined){
 
-			//if where is a valid point in the array of values, then proceed, otherwise return false
-			if(values[where] !== undefined){
+					faceupValuesArr.push(lifted);
 
-				//if there is a value already in our array of face up values
-				if(faceupValuesArr[0]){
+					board.splice(where,1, 'face up');
 
-					//if the value found in our face up array called by our faceupValue function is not equal to our where value
-					if(!(this.faceupValue() === values[where])){
+					if(displayFn){
 
-						//then push our value in our face up Array back into our original values array and remove it from the faceup array
-						values.push(faceupValuesArr.shift())
-
-						//then put our where value into our faceup array
-						faceupValuesArr.unshift(values[where]);
-
-						//where our where value was found in our values array, replace it with the string face up.
-						values[where] = 'face up';
+						return displayFn(faceupValuesArr[0]);
 					}
 					else{
 
-						delete(value[where] && faceupValuesArr[0]);
+						return this.faceupValue();
 					}
 				}
 
+				else if(matchFn(lifted, this.faceupValue())){
+
+					console.log(lifted + ', ' + this.faceupValue());
+
+					board.splice(where,1);
+
+					faceupValuesArr.shift();
+
+					board.splice(board.indexOf('face up'),1);
+
+					return 'Match!';
+					}
+				else{
+
+					displayFn(lifted);
+
+					faceupValuesArr.shift();
+
+					return 'no match! Try again';
+				}
 			}
-			return false;
-		}
+							
+			if(board.length === 0){
+				
+				if(endgameFn){
+
+					return endgameFn();
+				}
+				else{
+					return "No more pairs! You Won!";
+				}
+			}
 		
-		var faceupValuesArr = [];
+		};
+
+		
+		
+		function shuffle(array) {
+		  var m = array.length, t, i;
+
+		  // While there remain elements to shuffle…
+		  while (m) {
+
+		    // Pick a remaining element…
+		    i = Math.floor(Math.random() * m--);
+
+		    // And swap it with the current element.
+		    t = array[m];
+		   
+		    array[m] = array[i];
+		   
+		    array[i] = t;
+		  }
+		}
 	}
 	//...
 
@@ -66,12 +133,13 @@ var MemoryBoard = (function() {
 
 // card values:
 var food = ['apple','artichoke','banana','blueberry','carrot','cranberry'];
-// match callback:
+
+match callback:
 var sameInitial = function(str1,str2) {
 	return str1[0]===str2[0];
-}
+};
 // end game callback:
-var gameoverFn = function() {console.log("You win!")}
+var gameoverFn = function() {console.log("You win!");};
 
 var game = new MemoryBoard(food,sameInitial,null,gameoverFn);
 
@@ -89,3 +157,15 @@ var showName = function(animal) { // display only name:
 }
 
 var game = new MemoryBoard(animals,sameSpecies,showName);
+
+
+
+
+
+
+
+
+
+
+
+
